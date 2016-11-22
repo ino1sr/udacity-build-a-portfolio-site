0b5a2cc8f8d823267b9ce718aae0a33f5de3414a
Meine Goy <meine@goyman.com>
2016-11-23 00:15:47 +0100

    Base skeleton

---
+++ brunch-config.js
@@ -0,0 +1,63 @@
+exports.config = {
+  // See http://brunch.io/#documentation for docs.
+  files: {
+    javascripts: {
+      joinTo: "js/app.js"
+    },
+    stylesheets: {
+      joinTo: "css/app.css"
+    },
+    templates: {
+      joinTo: "js/app.js"
+    }
+  },
+
+  conventions: {
+    ignored: [
+      /[\\/]_/,
+      /.*\.d\.ts$/
+    ]
+  },
+
+  paths: {
+    // Dependencies and current project directories to watch
+    watched: [
+      "app",
+      "assets"
+    ],
+
+    // Where to compile files to
+    public: "_build"
+  },
+
+  plugins: {
+    sass: {
+      options: {
+        // Use includePaths to allow sass to load files outside your tree
+        // For example, from node_modules
+        //includePaths: ['app/css']
+      }
+    },
+    postcss: {
+      processors: [
+        require('autoprefixer')(['last 8 versions'])
+      ]
+    },
+    brunchTypescript: {
+      ignoreErrors: [
+        // https://github.com/baptistedonaux/brunch-typescript/issues/18
+        2503 // Cannot find namespace {0}
+      ]
+    }
+  },
+
+  modules: {
+    autoRequire: {
+      "js/app.js": ["js/app"]
+    }
+  },
+
+  npm: {
+    enabled: true
+  }
+};
